3dd408b9225e58e65eb7cc2b760652ffaefcf70a
Ayush Dhoot <101291613+Ayush4221@users.noreply.github.com>
2024-08-04 12:25:53 +0530

    without swap route

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,11 @@
+import { authConfig } from "@/app/lib/auth";
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google";
-const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
-        })
-      ]
-})
 
-export { handler as GET , handler as POST}
+const handler = NextAuth(authConfig)
+
+export { handler as GET, handler as POST }
+
+console.log({
+    clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+})
